08f7f02c0b13e90387cc8812e1abafa772d44c1c
kanwal <kaurkanwal@gmail.com>
2021-10-31 20:21:54 -0600

    added testcases, new requirement and refactor

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,54 +9,41 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            QualityCalculatorInterface calculator = getCalculator(items[i]);
+            if (calculator != null) {
+                items[i].quality = QualityCalculatorInterface
+                    .calculate(items[i].quality , calculator.changeQuality(items[i]));
                 items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
             }
         }
     }
+
+
+    private QualityCalculatorInterface getCalculator(Item item) {
+
+        QualityCalculatorInterface calculator = (Item it) -> it.sellIn > 0? 1: 2;
+
+        if (item.name.equals("Aged Brie"))  {
+            calculator = (Item it) -> it.sellIn > 0? -1: -2;
+
+        }
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            calculator = (Item it) -> {
+                if(it.sellIn <= 0) return it.quality;
+                if(it.sellIn <= 5) return -3;
+                if(it.sellIn <=10) return - 2;
+                return -1;
+            };
+        }
+
+        if(item.name.equals("Sulfuras, Hand of Ragnaros")){
+            calculator = null;
+        }
+
+        if(item.name.equals("Conjured Mana Cake")){
+            calculator = (Item it) -> it.sellIn > 0? 2: 4;
+        }
+        return calculator;
+    }
+
 }
